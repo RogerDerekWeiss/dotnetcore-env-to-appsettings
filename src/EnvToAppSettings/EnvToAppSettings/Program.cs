2bd0d68c165176af663cc308688d803bd70f387b
Roger Weiss <rogerderek.weiss@absa.africa>
2021-01-05 14:44:53 +0200

    Initial commit of project

---
+++ src/EnvToAppSettings/EnvToAppSettings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnvToAppSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Grab the file path from args
+            Console.WriteLine($"Using file: {args[0]}");
+            
+            
+        }
+    }
+}
